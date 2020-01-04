97388bf2809570dea7110e24746a926bc32cefc3
Lars Walther <meinkraeks@gmail.com>
2020-01-04 16:31:39 +0100

    fields for todo task

--- src/edi/documentation/content/todo_task.py
+++ src/edi/documentation/content/todo_task.py
@@ -1,61 +1,25 @@
 # -*- coding: utf-8 -*-
 # from plone.app.textfield import RichText
-# from plone.autoform import directives
 from plone.dexterity.content import Container
-# from plone.namedfile import field as namedfile
 from plone.supermodel import model
-# from plone.supermodel.directives import fieldset
-# from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
-
-
-# from edi.documentation import _
 
 
 class ITodoTask(model.Schema):
     """ Marker interface and Dexterity Python Schema for TodoTask
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('todo_task.xml')
+    title = schema.TextLine(title="ToDo (Was?)")
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    description = schema.Text(title="Weitere Beschreibung zur ToDo Augabe")
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    datetime = schema.Datetime(title="Wann?", required=False)
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
+    responsible = schema.TextLine(title="Wer?" required = False)
 
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
-
+    excludeFromDisplay = schema.Bool(title="ToDo Aufgabe von den normalen Ordneransichten ausschließen",
+                                     default=True,)
 
 @implementer(ITodoTask)
 class TodoTask(Container):
