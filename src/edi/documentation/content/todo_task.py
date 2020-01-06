ea9ca5b12af46aeb911912400b21876c5a7d2352
Lars Walther <meinkraeks@gmail.com>
2020-01-06 10:59:59 +0100

    Changes

--- src/edi/documentation/content/todo_task.py
+++ src/edi/documentation/content/todo_task.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
+from plone.app.textfield import RichText
 from plone.dexterity.content import Container
 from plone.supermodel import model
 from zope import schema
 from zope.interface import implementer
+from DateTime import DateTime
+from plone.indexer import indexer
 
 
 class ITodoTask(model.Schema):
@@ -12,11 +14,15 @@ class ITodoTask(model.Schema):
 
     title = schema.TextLine(title=u"ToDo (Was?)")
 
-    description = schema.Text(title=u"Weitere Beschreibung zur ToDo Augabe")
+    description = schema.Text(title=u"Weitere Beschreibung der ToDo Aufgabe")
 
-    datetime = schema.Datetime(title=u"Wann?", required=False)
+    datetime = schema.Datetime(title=u"Wann?", required=True)
+
+    referenz = schema.URI(title=u"Referenz-URL zu dieser Aufgabe", description=u"z.B: URL auf Github", required=False)
 
     responsible = schema.TextLine(title=u"Wer?", required = False)
+
+    erledigung = RichText(title=u"Dokumentation zur Erledigung der ToDo Aufgabe", required = False)
 
     excludeFromDisplay = schema.Bool(title=u"ToDo Aufgabe von den normalen Ordneransichten ausschließen",
                                      default=True,)
@@ -25,3 +31,9 @@ class ITodoTask(model.Schema):
 class TodoTask(Container):
     """
     """
+
+@indexer(ITodoTask)
+def endIndexer(obj):
+    if obj.datetime is None:
+        return None
+    return DateTime(obj.datetime)
